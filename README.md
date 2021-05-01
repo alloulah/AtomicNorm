31cfb5a6358ea86d94f08935dfb3e866d5b8051a
alloulah <alloulah@outlook.com>
2021-05-01 13:57:52 +0100

    Access to dual solution via correct CVX syntax. Removing regularisation and multitone implementation for now.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AtomicNorm
 
-This repo contains a vanilla Matlab implementation of 1-dimensional spectal estimation via Atomic Norm. Based on original work by [Bhaskar et al.](https://arxiv.org/pdf/1204.0562) and extension for multi-measurement snapshots by [Li and Chi](https://arxiv.org/pdf/1408.2242).
+This repo contains a vanilla Matlab implementation of 1-dimensional spectal estimation via Atomic Norm. Based on original work by [Bhaskar et al.](https://arxiv.org/pdf/1204.0562).
 
 Installation of convex optimisation toolbox [CVX](http://cvxr.com/cvx/) is assumed. Alternatively, run script `set_path` to use toolbox, assuming it is available in your workspace.
+
+Any issues, contact me at alloulah@outlook.com.
--- atomicNorm_spectralEst_1d_tb.m
+++ atomicNorm_spectralEst_1d_tb.m
@@ -20,37 +20,26 @@ clc
 
 N = 256;
 
-sigmaSnr_dB = 40;
 
 nn = [0:N-1].';
-f = [-36 5 15];
+% f = [-36 36];   % multi-tone
+f = [36];       % single-tone
 s = sum(exp(1i*2*pi*f.*nn/N), 2);
 
-% normalise power level as required
-normPwr_dB = 0;
-pwr_dB = 10*log10(s'*s/length(s));
-sigScale = sqrt(10^(-(pwr_dB-normPwr_dB)/10));
-s = sigScale * s;
-sigPwr_dB = 10*log10(s'*s/length(s));
-% add Gaussian white noise - SNR [dB]
-n_awgn = 10^(-sigmaSnr_dB/20)*(randn(size(s)) + 1i*randn(size(s)))/sqrt(2);
-noisePwr_dB = 10*log10(n_awgn'*n_awgn/length(n_awgn));
-s = s + n_awgn;
-fprintf('SNR = %2.3f (dB)\n', sigPwr_dB-noisePwr_dB);
 
 % inspect Fourier spectrum
 t_f = nn - N/2;
-s_f = fftshift(fft(s));
+spectr_fft = fftshift(fft(s));
 figure();
-plot( t_f, 20*log10( abs(s_f) ) )
+plot( t_f, 20*log10( abs(spectr_fft) ) )
 xlim([t_f(1) t_f(end)])
 xlabel('freq (Hz)')
 ylabel('spectrum (dB)')
 
 %% Atomic Norm
 
-% regularizer is a function of SNR
-tau = 10^(-sigmaSnr_dB/20)*(1 + 1/log(N))*sqrt( N*log(N)*4*pi*log(N) );
+% regularizer is off
+tau = 1;
 
 y = s;
 
@@ -58,6 +47,7 @@ y = s;
 cvx_begin sdp %quiet
 
 variable x_est(N) complex
+dual variable x_dual
 variable t
 variable u(1,N) complex
 
@@ -65,16 +55,17 @@ Z = toeplitz(u);
 
 H = [Z x_est;x_est' t];
 
-minimize(sum_square_abs(y-x_est)/2 + (tau/2)*(real(trace(Z)) + t))
+minimize( sum_square_abs(y-x_est)/2 + (tau/2)*(real(trace(Z))/N + t) ) %
 subject to
 H == hermitian_semidefinite(N+1)
+x_dual : x_est == y
 
 cvx_end
 
-% dual solution
-x_hat = y - x_est;
-
 %% Localise Frequencies
+
+% dual solution
+x_hat = x_dual;
 
 % discrete set of Atoms (Fourier)
 A = zeros(N, N);
@@ -83,24 +74,18 @@ for f = -N/2:N/2-1
 end
 spectr_an = x_hat'*A;
 
+
+spectr0_fft_dB = 20*log10( abs(spectr_fft) );
+spectr_an_dB = 20*log10( abs(spectr_an) );
+% normalise FFT spectrum w.r.t. AN's
+spectr_fft_dB = spectr0_fft_dB - max(spectr0_fft_dB);
+
 % should be also equivalent to FFT
-spectr_fft = fftshift(fft(x_hat));
 figure();
 hold on
-plot( t_f, 20*log10( abs(spectr_an) ) )
-plot( t_f, 20*log10( abs(spectr_fft) ), '--' )
+plot( t_f, spectr_an_dB )
+plot( t_f, spectr_fft_dB, '--' )
 hold off
 xlim([t_f(1) t_f(end)])
 xlabel('freq (Hz)')
 ylabel('spectrum (dB)')
-
-%% Inspect regularisation as a function of SNR
-
-snr_vect = [0:50];
-tau_vect = 10.^(-snr_vect/20)*(1 + 1/log(N))*sqrt( N*log(N)*4*pi*log(N) );
-
-figure();
-plot( snr_vect, tau_vect )
-xlim([snr_vect(1) snr_vect(end)])
-xlabel('SNR (dB)')
-ylabel('\tau')
